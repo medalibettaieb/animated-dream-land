ef80733c96d78187f449a77aae9b77ae00370b0e
bettaieb <bettaieb@DESKTOP-KM2D64Q>
2018-03-17 23:24:07 +0000

    tt
    Signed-off-by: bettaieb <bettaieb@DESKTOP-KM2D64Q>

---
+++ dream-land/dream-land-client/src/main/java/tn/bettaieb/dream_land/app/client/gui/productTests/TestFindUsersByPack.java
@@ -0,0 +1,48 @@
+package tn.bettaieb.dream_land.app.client.gui.productTests;
+
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import tn.bettaieb.dream_land.entities.Pack;
+import tn.bettaieb.dream_land.entities.User;
+import tn.bettaieb.dream_land.services.AmusementServiceRemote;
+import tn.bettaieb.dream_land.services.AssignementServiceRemote;
+import tn.bettaieb.dream_land.services.EquipementServiceRemote;
+import tn.bettaieb.dream_land.services.PackServiceRemote;
+import tn.bettaieb.dream_land.services.ReportingServiceRemote;
+import tn.bettaieb.dream_land.services.TicketingServiceRemote;
+import tn.bettaieb.dream_land.services.UserServiceRemote;
+
+public class TestFindUsersByPack {
+
+	public static void main(String[] args) throws NamingException {
+		Context context = new InitialContext();
+		AssignementServiceRemote assignementServiceRemote = (AssignementServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/AssignementService!tn.bettaieb.dream_land.services.AssignementServiceRemote");
+		EquipementServiceRemote equipementServiceRemote = (EquipementServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/EquipementService!tn.bettaieb.dream_land.services.EquipementServiceRemote");
+		AmusementServiceRemote amusementServiceRemote = (AmusementServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/AmusementService!tn.bettaieb.dream_land.services.AmusementServiceRemote");
+		TicketingServiceRemote ticketingServiceRemote = (TicketingServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/TicketingService!tn.bettaieb.dream_land.services.TicketingServiceRemote");
+		UserServiceRemote userServiceRemote = (UserServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/UserService!tn.bettaieb.dream_land.services.UserServiceRemote");
+		PackServiceRemote packServiceRemote = (PackServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/PackService!tn.bettaieb.dream_land.services.PackServiceRemote");
+		ReportingServiceRemote reportingServiceRemote = (ReportingServiceRemote) context.lookup(
+				"dream-land-ear/dream-land-service/ReportingService!tn.bettaieb.dream_land.services.ReportingServiceRemote");
+
+		Pack pack = packServiceRemote.find(1);
+
+		List<User> customers = reportingServiceRemote.findUsersByPack(pack);
+
+		for (User u : customers) {
+			System.out.println(u.getName());
+		}
+
+	}
+
+}
